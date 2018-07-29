3b9c6fe6e1beb12e824608fb1fdfbfbea5ef777b
Chen Lutong <936664530@qq.com>
2018-07-29 17:21:03 +0800

    123
    123123

--- igem-run.sh
+++ igem-run.sh
@@ -1,29 +1,7 @@
 #!/bin/bash
 workdir=/Users/ertuil/workspace/igem/app-test1
+
 docker_name=igem-dev
-
-function isexist()
-{
-    source_str=$1
-    test_str=$2
-    
-    strings=$(echo $source_str | sed 's/:/ /g')
-    for str in $strings
-    do  
-        if [ $test_str=$str ]; then
-            return 0
-        fi  
-    done
-    return 1
-}
-
-PPWD=`pwd` 
-if isexist $PATH $PPWD; then 
-    echo "no need to set it ..."
-else
-    echo $PPWD
-    #export PATH=$PPWD:$PATH
-fi
 
 
 if [ $# -ge 1 ]; then
